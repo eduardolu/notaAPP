f1b26b9c46d00fa00faddd36e3ee832d1e6d5101
eduardolu <edu291086@gmail.com>
2023-01-13 23:07:39 +0100

    finalizar parte de view

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,5 @@
-import { Typography } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView } from "../views/NoteView"
 import { NothingSelectedView } from "../views/NothingSelectedView"
@@ -8,8 +9,23 @@ export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>texto para rellenar para comprobar las cosas</Typography> */}
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color:'while',
+          backgroundColor: 'error.main',
+          ':hover':{backgroundColor:'error.main', opacity: 0.8},
+          position: 'fixed',
+          right:50,
+          bottom:50
+        }}
+      >
+        <AddOutlined sx={{fontSize: 35}}/>
+      </IconButton>
+
     </JournalLayout>
   )
 }
