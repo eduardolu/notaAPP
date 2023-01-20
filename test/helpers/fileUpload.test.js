e362d3d909b63860da0ae00dd353009df9831b65
eduardolu <edu291086@gmail.com>
2023-01-20 20:12:32 +0100

    sin error de despliege

---
+++ test/helpers/fileUpload.test.js
@@ -0,0 +1,11 @@
+import { fileUpload } from "../../src/helpers/fileUpload"
+
+describe('fileUpload', () => { 
+    test('should carga de imagen', async() => { 
+        const imageUrl = 'https://noticias.mapfre.com/media/2020/03/Fondos-1.jpg'
+        
+        
+        const url = await fileUpload()
+     })
+
+ })
