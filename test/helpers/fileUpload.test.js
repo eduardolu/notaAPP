0a30102cee6e8e900ec23265ae8b62319135c6d1
eduardolu <edu291086@gmail.com>
2023-03-09 16:00:15 +0100

    actualizaciones

--- test/helpers/fileUpload.test.js
+++ test/helpers/fileUpload.test.js
@@ -2,10 +2,13 @@ import { fileUpload } from "../../src/helpers/fileUpload"
 
 describe('fileUpload', () => { 
     test('should carga de imagen', async() => { 
-        const imageUrl = 'https://noticias.mapfre.com/media/2020/03/Fondos-1.jpg'
+        const imageUrl = 'https://res.cloudinary.com/ddprcgs6t/image/upload/v1674243348/diario-app/notas/p1yjuhsi9apqplulvi6c.png'
+        const resp = await fetch( imageUrl );
+        const blob = await resp.blob();
+        const file = new File([blob],'foto.jpg')
         
-        
-        const url = await fileUpload()
+        const url = await fileUpload(file)
+        expect (typeof url).toBe ('string')
      })
 
  })
